ebb9ec105cbac725f642373bf015da80070ed501
hslape <awinter71417@gmail.com>
2024-04-10 15:21:04 -0500

    Add product list component to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
+import { ProductListComponent } from "./product-list/product-list.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    imports: [CommonModule, RouterOutlet, NavbarComponent]
+    imports: [CommonModule, RouterOutlet, NavbarComponent, ProductListComponent]
 })
 export class AppComponent {
   title = 'phatloot_client_2';
