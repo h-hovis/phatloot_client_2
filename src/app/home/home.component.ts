b0f519524159269c34d3682b24932f114fc3c365
hslape <awinter71417@gmail.com>
2024-04-14 15:13:17 -0500

    Update home component and app component to include product list component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductListComponent } from "../product-list/product-list.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css'],
+    imports: [CommonModule, ProductListComponent]
 })
 export class HomeComponent {
 
